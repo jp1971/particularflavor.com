b86b25288b1a59df02011884a45064979c24fff8
Jameson Proctor <jameson@athleticsnyc.com>
2015-03-21 08:55:37 -0400

    Adding Bower post install script.

---
+++ bin/bower/post_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Bower postinstall hook
+# @see https://github.com/bower/bower/blob/master/HOOKS.md
+
+## Copy RequireJS to public theme directory
+cp wp-content/themes/mnfst-child-theme/bower_components/requirejs/require.js wp-content/themes/mnfst-child-theme/assets/js/
+
+## Copy Throttle/Debounce to the public theme directory
+cp wp-content/themes/mnfst-child-theme/bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.min.js wp-content/themes/mnfst-child-theme/js/vendor/
