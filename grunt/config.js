a0b1f767f875a81d61b2a71bd679395d6b7b0c20
Jameson Proctor <jameson@athleticsnyc.com>
2015-03-21 08:56:24 -0400

    Replacing references to other projects.

--- grunt/config.js
+++ grunt/config.js
@@ -1,10 +1,10 @@
-var project = 'tsw';
+var project = 'mnfst-child-theme';
 
 module.exports = {
 
 	project: project,
 	paths: {
-		krnl: 'wp-content/themes/_krnl',
+		krnl: 'wp-content/themes/_mnfst',
 		theme: 'wp-content/themes/' + project
 	}
 
